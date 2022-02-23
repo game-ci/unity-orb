4da208afed559c12bf079302517443617626a13a
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-02-23 13:48:20 -0800

    add another condition for license check
    fix log name
    
    add changes
    
    first commit of the build command
    
    Add GameCI's builder as the default method
    
    change curl identation
    
    move the store artifact from command to job
    
    add the build per se to the command
    
    add a couple of todo for reference
    
    Trigger pipeline
    
    add GitHub flow
    
    Trigger pipeline
    
    Trigger pipeline
    
    got a running build
    
    small refactor

---
+++ src/scripts/before-script.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+readonly base_dir="${CIRCLE_WORKING_DIRECTORY/\~/$HOME}"
+
+# Download GameCI's script responsible for preparing the environment.
+download_before_script() {
+  curl --silent --location \
+    --request GET \
+    --url "https://gitlab.com/game-ci/unity3d-gitlab-ci-example/-/raw/main/ci/before_script.sh" \
+    --header 'Accept: application/vnd.github.v3+json' \
+    --output "$base_dir/before_script.sh"
+}
+
+if ! download_before_script; then
+  printf '%s\n' "Failed to download \"before_script.sh\"."
+  exit 1
+fi
+
+chmod +x "$base_dir/before_script.sh"
+
+# Decode Unity license.
+readonly encoded_unity_license="${!UNITY_LICENSE_VAR_NAME}"
+readonly decoded_unity_license=$(printf '%s\n' "$encoded_unity_license" | base64 --decode)
+
+# Nomenclature required by the script.
+readonly UNITY_LICENSE="$decoded_unity_license"
+
+source "$base_dir/before_script.sh"
