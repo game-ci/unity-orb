559ae5b07761fef07a633fcfeacb36dd1837dc52
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-02-23 13:48:20 -0800

    add another condition for license check

--- src/scripts/activate.sh
+++ src/scripts/activate.sh
@@ -17,26 +17,25 @@ if [[ -z "$ENCODED_UNITY_LICENSE" ]]; then
     exit 1
 fi
 
-# Can I assume the lib "base64" is available? Should I "command -v base64" first?
 DECODED_UNITY_LICENSE=$(stdmsg "$ENCODED_UNITY_LICENSE" | base64 --decode)
 
-# Saving decoded ULF to file and activating unity
+# Writing the decoded license to a temporary file
 TMP_UNITY_DIR=$(mktemp -d 'unity-orb.XXXXXX')
 TMP_UNITY_LICENSE_FILE="$TMP_UNITY_DIR/license.ulf"
 touch "$TMP_UNITY_LICENSE_FILE"
 chmod 0600 "$TMP_UNITY_LICENSE_FILE"
 
-stdmsg "Saving the decoded license in: ${TMP_UNITY_LICENSE_FILE}"
+stdmsg "Writing the decoded license to \"${TMP_UNITY_LICENSE_FILE}\""
 stdmsg "$DECODED_UNITY_LICENSE" > "$TMP_UNITY_LICENSE_FILE"
 
 # Returning "true" after activation to bypass misleading exit code 1
 "$UNITY_EDITOR" -batchmode -manualLicenseFile "$TMP_UNITY_LICENSE_FILE" -logfile "$TMP_UNITY_DIR/activation-log.txt" || true
 if [[ "$PARAM_UNITY_VERBOSE" -eq 1 ]]; then cat "$TMP_UNITY_DIR/activation-log.txt"; fi
 
-if grep "License file loaded" "$TMP_UNITY_DIR/activation-log.txt"; then
+if grep "License file loaded" "$TMP_UNITY_DIR/activation-log.txt" && grep "Next license update check is after" "$TMP_UNITY_DIR/activation-log.txt"; then
     stdmsg "Unity activated successfully."
 else
     errmsg "Error activating Unity."
-    errmsg "See more details by running with \"verbose\" set to true or try generating the license again."
+    errmsg "Run the job with \"verbose\" set to true for more details or try generating the license again."
     exit 1
 fi
