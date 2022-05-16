fd84083982995bc550602cf1f275605995c0b774
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-05-16 11:07:18 -0700

    feat: add cache to runner

--- src/scripts/windows/build.sh
+++ src/scripts/windows/build.sh
@@ -11,15 +11,17 @@ docker exec "$container_name" powershell mkdir C:/build
 docker exec "$container_name" powershell "[System.Environment]::SetEnvironmentVariable('BUILD_NAME','$PARAM_BUILD_NAME', [System.EnvironmentVariableTarget]::Machine)"
 docker exec "$container_name" powershell "[System.Environment]::SetEnvironmentVariable('BUILD_TARGET','$PARAM_BUILD_TARGET', [System.EnvironmentVariableTarget]::Machine)"
 
-# Run the scripts
+# Build the project
 docker exec "$container_name" powershell '& "C:\Program Files\Unity\Hub\Editor\2021.3.2f1\Editor\Unity.exe" -batchmode -quit -nographics -projectPath $Env:PROJECT_PATH -buildTarget $Env:BUILD_TARGET -customBuildTarget $Env:BUILD_TARGET -customBuildName $Env:BUILD_NAME -customBuildPath "C:/build/" -buildVersion "1.0.0" -executeMethod BuildCommand.PerformBuild -logfile | Out-Host'
+
+# Compress the build and Library folder.
 docker exec "$container_name" powershell 'tar -czf "C:/$Env:BUILD_NAME-$Env:BUILD_TARGET.tar.gz" -C "C:/build" .'
+docker exec "$container_name" powershell 'tar -czf "C:/library.tar.gz" -C "C:/unity_project/Library" .'
 
-# Copy the build to the host
+# Copy the build and Library to the host.
 docker cp "$container_name":"$PARAM_BUILD_NAME"-"$PARAM_BUILD_TARGET".tar.gz "$base_dir"/"$PARAM_BUILD_NAME"-"$PARAM_BUILD_TARGET".tar.gz
+docker cp "$container_name":library.tar.gz "$base_dir"/library.tar.gz
 
-# Copy the Library to the host
+# Update Library folder to update the cache.
 rm -rf "$unity_project_full_path"/Library
-
-# shellcheck disable=SC2140
-docker cp "$container_name":"C:/unity_project/Library" "$base_dir"/unity_project/Library
+tar -xzf "$base_dir"/library.tar.gz -C "$unity_project_full_path/Library" --strip-components 1
