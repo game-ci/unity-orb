f3639a15c0e6071ec0a0d2e4bccfee08d2844539
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-05-16 10:54:06 -0700

    fix

--- src/scripts/windows/build.sh
+++ src/scripts/windows/build.sh
@@ -8,8 +8,8 @@ readonly container_name="unity_container"
 docker exec "$container_name" powershell mkdir C:/build
 
 # Add the build target and build name in the environment variables.
-docker exec "$container_name" powershell "\$Env:BUILD_NAME = '$PARAM_BUILD_NAME'"
-docker exec "$container_name" powershell "\$Env:BUILD_TARGET = '$PARAM_BUILD_TARGET'"
+docker exec "$container_name" powershell "[System.Environment]::SetEnvironmentVariable('BUILD_NAME','$PARAM_BUILD_NAME', [System.EnvironmentVariableTarget]::Machine)"
+docker exec "$container_name" powershell "[System.Environment]::SetEnvironmentVariable('BUILD_TARGET','$PARAM_BUILD_TARGET', [System.EnvironmentVariableTarget]::Machine)"
 
 # Run the scripts
 docker exec "$container_name" powershell '& "C:\Program Files\Unity\Hub\Editor\2021.3.2f1\Editor\Unity.exe" -batchmode -quit -nographics -projectPath $Env:PROJECT_PATH -buildTarget $Env:BUILD_TARGET -customBuildTarget $Env:BUILD_TARGET -customBuildName $Env:BUILD_NAME -customBuildPath "C:/build/" -buildVersion "1.0.0" -executeMethod BuildCommand.PerformBuild -logfile | Out-Host'
@@ -17,3 +17,7 @@ docker exec "$container_name" powershell 'tar -czf "C:/$Env:BUILD_NAME-$Env:BUIL
 
 # Copy the build to the host
 docker cp "$container_name":"$PARAM_BUILD_NAME"-"$PARAM_BUILD_TARGET".tar.gz "$base_dir"/"$PARAM_BUILD_NAME"-"$PARAM_BUILD_TARGET".tar.gz
+
+# Copy the Library to the host
+rm -rf "$unity_project_full_path"/Library
+docker cp "$container_name":"C:/unity_project/Library" "$base_dir"/unity_project/Library
