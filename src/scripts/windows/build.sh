615450c814b29da8509ced1993b546cd4d81a63d
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-06-23 13:21:41 -0700

    chore: add version comment to the windows build command

--- src/scripts/windows/build.sh
+++ src/scripts/windows/build.sh
@@ -24,7 +24,9 @@ docker exec "$CONTAINER_NAME" powershell "[System.Environment]::SetEnvironmentVa
 docker exec "$CONTAINER_NAME" powershell "[System.Environment]::SetEnvironmentVariable('BUILD_TARGET','$PARAM_BUILD_TARGET', [System.EnvironmentVariableTarget]::Machine)"
 
 # Build the project
-docker exec "$CONTAINER_NAME" powershell '& "C:\Program Files\Unity\Hub\Editor\*\Editor\Unity.exe" -batchmode -quit -nographics -projectPath $Env:PROJECT_PATH -buildTarget $Env:BUILD_TARGET -customBuildTarget $Env:BUILD_TARGET -customBuildName $Env:BUILD_NAME -customBuildPath "C:/build/" -buildVersion "1.0.0" -executeMethod BuildCommand.PerformBuild -logfile | Out-Host'
+# Versioning of the project needs work. This is how it's done in the GHA:
+# https://github.com/game-ci/unity-builder/blob/main/src/model/versioning.ts
+docker exec "$CONTAINER_NAME" powershell '& "C:\Program Files\Unity\Hub\Editor\*\Editor\Unity.exe" -batchmode -quit -nographics -projectPath $Env:PROJECT_PATH -buildTarget $Env:BUILD_TARGET -customBuildTarget $Env:BUILD_TARGET -customBuildName $Env:BUILD_NAME -customBuildPath "C:/build/" -executeMethod BuildCommand.PerformBuild -logfile | Out-Host'
 
 # Compress the build and Library folder.
 docker exec "$CONTAINER_NAME" powershell 'tar -czf "C:/$Env:BUILD_NAME-$Env:BUILD_TARGET.tar.gz" -C "C:/build" .'
