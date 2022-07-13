5d87d0dc044b75946e6b1078c2ae3e9351e55f9d
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-07-13 20:39:07 -0300

    refactor: clean unecessary logic from Windows `build` (#19)

--- src/scripts/windows/build.sh
+++ src/scripts/windows/build.sh
@@ -28,17 +28,11 @@ docker exec "$CONTAINER_NAME" powershell "[System.Environment]::SetEnvironmentVa
 # https://github.com/game-ci/unity-builder/blob/main/src/model/versioning.ts
 docker exec "$CONTAINER_NAME" powershell '& "C:\Program Files\Unity\Hub\Editor\*\Editor\Unity.exe" -batchmode -quit -nographics -projectPath $Env:PROJECT_PATH -buildTarget $Env:BUILD_TARGET -customBuildTarget $Env:BUILD_TARGET -customBuildName $Env:BUILD_NAME -customBuildPath "C:/build/" -executeMethod BuildCommand.PerformBuild -logfile | Out-Host'
 
-# Compress the build and Library folder.
+# Compress the build folder.
 docker exec "$CONTAINER_NAME" powershell 'tar -czf "C:/$Env:BUILD_NAME-$Env:BUILD_TARGET.tar.gz" -C "C:/build" .'
-docker exec "$CONTAINER_NAME" powershell 'tar -czf "C:/library.tar.gz" -C "C:/unity_project/Library" .'
 
-# Copy the build and Library directories to the host.
+# Copy the build directory to the host.
 docker cp "$CONTAINER_NAME":"$PARAM_BUILD_NAME"-"$PARAM_BUILD_TARGET".tar.gz "$base_dir"/"$PARAM_BUILD_NAME"-"$PARAM_BUILD_TARGET".tar.gz
-docker cp "$CONTAINER_NAME":library.tar.gz "$base_dir"/library.tar.gz
-
-# Update Library folder to update the cache.
-# rm -rf "$unity_project_full_path"/Library
-tar -xzf "$base_dir"/library.tar.gz -C "$unity_project_full_path"
 
 # Remove the container.
 docker rm -f "$CONTAINER_NAME"
