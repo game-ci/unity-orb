4c9e33a8090639029f2f5e8ddad354b81cffd694
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-09-20 14:33:02 -0700

    feat: add check for `build-method` in the build entry point.

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -9,9 +9,12 @@ eval "$SCRIPT_UTILS"
 # Detect host OS.
 detect-os
 
-# Copy builder to project directory.
-mkdir -p "$unity_project_full_path/Assets/Editor/"
-printf '%s\n' "$DEPENDENCY_UNITY_BUILDER" > "$unity_project_full_path/Assets/Editor/BuildCommand.cs"
+# Copy builder to project directory if a custom isn't specified.
+if [ -z "$PARAM_BUILD_METHOD" ]; then
+  printf '%s\n' "The \"build-method\" parameter is empty. Falling back to the default build script."
+  mkdir -p "$unity_project_full_path/Assets/Editor/"
+  printf '%s\n' "$DEPENDENCY_UNITY_BUILDER" > "$unity_project_full_path/Assets/Editor/BuildCommand.cs"
+fi
 
 # If "build_name" is blank, use the build target.
 if [ -z "$PARAM_BUILD_NAME" ]; then PARAM_BUILD_NAME="$PARAM_BUILD_TARGET"; fi
