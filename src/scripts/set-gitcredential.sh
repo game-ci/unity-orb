dedc18bbee301239ca93fc37a5a6883fb75e7045
Jt Gleason <jt@entropyfails.com>
2023-11-30 14:01:29 -0600

    Properly handle the different environments

--- src/scripts/set-gitcredential.sh
+++ src/scripts/set-gitcredential.sh
@@ -14,39 +14,7 @@ detect-os
 readonly git_private_token="${!PARAM_GIT_PRIVATE_TOKEN}"
 readonly CONTAINER_NAME="${CIRCLE_PROJECT_REPONAME}-${CIRCLE_BUILD_NUM}"
 
-trap_exit() {
-    local exit_status="$?"
-    
-    if [ "$exit_status" -ne 0 ]; then
-        printf '%s\n' 'The script did not complete successfully.'
-        
-        printf '%s\n' "Removing the container \"$CONTAINER_NAME\"."
-        docker rm -f "$CONTAINER_NAME" &> /dev/null || true
-        
-        exit "$exit_status"
-    fi
-}
-trap trap_exit EXIT
-
-
-if [ -z "${git_private_token}" ]
-then
-    echo "GIT_PRIVATE_TOKEN unset skipping"
-else
-    echo "GIT_PRIVATE_TOKEN is set configuring git credentials"
-    
-    docker exec "$CONTAINER_NAME" "git config --global credential.helper store"
-    docker exec "$CONTAINER_NAME" "git config --global --replace-all url.\"https://token:$git_private_token@github.com/\".insteadOf ssh://git@github.com/"
-    docker exec "$CONTAINER_NAME" "git config --global --add url.\"https://token:$git_private_token@github.com/\".insteadOf git@github.com"
-    docker exec "$CONTAINER_NAME" "git config --global --add url.\"https://token:$git_private_token@github.com/\".insteadOf \"https://github.com/\""
-    
-    docker exec "$CONTAINER_NAME" "git config --global url.\"https://ssh:$git_private_token@github.com/\".insteadOf \"ssh://git@github.com/\""
-    docker exec "$CONTAINER_NAME" "git config --global url.\"https://git:$git_private_token@github.com/\".insteadOf \"git@github.com:\""
-    
+if [ "$PLATFORM" = "linux" ]; then eval "$SCRIPT_GIT_CREDENTIAL_LINUX";
+elif [ "$PLATFORM" = "macos" ]; then eval "$SCRIPT_GIT_CREDENTIAL_MACOS";
+elif [ "$PLATFORM" = "windows" ]; then eval "$SCRIPT_GIT_CREDENTIAL_WINDOWS";
 fi
-
-echo "---------- git config --list -------------"
-docker exec "$CONTAINER_NAME" "git config --list"
-
-echo "---------- git config --list --show-origin -------------"
-docker exec "$CONTAINER_NAME" "git config --list --show-origin"
